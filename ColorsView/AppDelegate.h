90d28ad11e17ef0c2cb26c0885f0b1abb7bc1b44
guoliting <guoliting@didichuxing.com>
2016-07-25 11:54:36 +0800

    初始哈工程

---
+++ ColorsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorsView
+//
+//  Created by zmit on 15/2/22.
+//  Copyright (c) 2015年 zmit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorsView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorsView
+//
+//  Created by zmit on 15/2/22.
+//  Copyright (c) 2015年 zmit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
